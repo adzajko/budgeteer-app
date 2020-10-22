e9b655364e81165c1d9134ffe7a4e26544ee86ee
adzajko <adzajkoski@gmail.com>
2020-10-23 01:04:50 +0200

    Added Ant UI
    Signed-off-by: adzajko <adzajkoski@gmail.com>

---
+++ budgeteer-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginComponent from "./components/login/LoginComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <LoginComponent/>
+        </div>
+    );
+}
+
+export default App;
